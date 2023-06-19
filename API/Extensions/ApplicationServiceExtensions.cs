1feb10e64aa16285886b5509ea51ee3bb9ea618d
sureshtrycatchlearn <sureshtrycatchlearn@gmail.com>
2023-06-19 14:43:14 +0300

    Lesson 159

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -9,6 +9,8 @@ using Microsoft.EntityFrameworkCore;
 using Persistence;
 using FluentValidation;
 using FluentValidation.AspNetCore;
+using Application.Interfaces;
+using Infrastructure.Security;
 
 namespace API.Extensions
 {
@@ -34,6 +36,8 @@ namespace API.Extensions
                 services.AddAutoMapper(typeof(MappingProfiles).Assembly);
                 services.AddFluentValidationAutoValidation();
                 services.AddValidatorsFromAssemblyContaining<Create>();
+                services.AddHttpContextAccessor();
+                services.AddScoped<IUserAccessor, UserAccessor>();
                 
 
                 return services;
--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Application.Activities.Core;
 using Domain;
 using FluentValidation;
 using MediatR;
 using Persistence;
+using Application.Activities.Core;
+using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Activities
 {
@@ -27,13 +25,24 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Command,Result<Unit>>
         {
         private readonly DataContext _context;
-            public Handler(DataContext context)
+        private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor )
             {
+            _userAccessor = userAccessor;
             _context = context;
             }
 
             public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
+                var user = await _context.Users.FirstOrDefaultAsync(x=>x.UserName==_userAccessor.GetUsername());
+
+                var attendee = new ActivityAttendee
+                {
+                    AppUser = user,
+                    Activity = request.Activity,
+                    IsHost = true
+                };
+
                 _context.Activities.Add(request.Activity);
 
                 var result= await _context.SaveChangesAsync()>0;
