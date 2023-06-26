2ba6908c2598a390260c047aebd36b67307be12a
sureshtrycatchlearn <sureshtrycatchlearn@gmail.com>
2023-06-26 17:09:58 +0300

    Today Commit

---
+++ Domain/UserFollowing.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class UserFollowing
+    {
+        public string ObserverId { get; set; }
+        public AppUser Observer { get; set; }
+        
+    }
+}
