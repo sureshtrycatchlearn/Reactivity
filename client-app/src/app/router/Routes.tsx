ba3868ed8d31f0a260040a4b95c1ad5d975cdc4c
sureshtrycatchlearn <sureshtrycatchlearn@gmail.com>
2023-07-01 10:42:31 +0300

    End of Lesson 252 Before Production

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -8,19 +8,21 @@ import NotFound from "../../features/errors/NotFound";
 import ServerError from "../../features/errors/ServerError";
 import LoginForm from "../../features/users/LoginForm";
 import ProfilePage from "../../features/profiles/ProfilePage";
+import RequireAuth from "./RequireAuth";
 
  export const routes:RouteObject[]=[
     {
         path:'/',
         element:<App/>,
         children:[
-            {path:'activities', element:<ActivityDashboard/>},
-            {path:'activities/:id', element:<ActivityDetails/>},
-            {path:'createActivity', element:<ActivityForm key='create'/>},
-            {path:'profiles/:username', element:<ProfilePage/>},
-            {path:'manage/:id', element:<ActivityForm key='manage'/>},
-            {path:'login', element:<LoginForm/>},
-            {path:'errors', element:<TestErrors/>},
+            {element:<RequireAuth/>, children: [
+                {path:'activities', element:<ActivityDashboard/>},
+                {path:'activities/:id', element:<ActivityDetails/>},
+                {path:'createActivity', element:<ActivityForm key='create'/>},
+                {path:'profiles/:username', element:<ProfilePage/>},
+                {path:'manage/:id', element:<ActivityForm key='manage'/>},
+                {path:'errors', element:<TestErrors/>},
+            ]},
             {path:'not-found', element:<NotFound/>},
             {path:'server-error', element:<ServerError/>},
             {path:'*', element:<Navigate replace to='/not-found'/>},
